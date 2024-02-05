df3116ca0d38857496aa6db0f93b818793f08429
DELL <nilesh.b@techroversolutions.com>
2024-02-05 14:57:45 +0530

    [BV]login css add

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './pages/auth/login/login.component';
+import { ForgotPasswordComponent } from './pages/auth/forgot-password/forgot-password.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: LoginComponent},
+  {path: 'app-forgot-password', component: ForgotPasswordComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
